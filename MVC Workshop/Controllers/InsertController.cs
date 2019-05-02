f0e7f16197e0c9a530cf3389df9e20f325387cb9
Mindy <1105137113@nkust.edu.tw>
2019-05-02 16:06:24 +0800

    加入專案檔案。

---
+++ MVC Workshop/Controllers/InsertController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Workshop.Controllers
+{
+    public class InsertController : Controller
+    {
+        // GET: Insert
+        public ActionResult InsertIndex()
+        {
+            return View();
+        }
+    }
+}
